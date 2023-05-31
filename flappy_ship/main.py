c6a486e2c19a73163268f7b10d19c3a79e445a10
lucasmv16 <84734581+lucasmv16@users.noreply.github.com>
2023-05-30 22:24:42 -0300

    fiz upload errado mais uma vez...

--- flappy_ship/main.py
+++
@@ -1,6 +0,0 @@
-import menus
-from menus.main_menu import MainMenu
-from game import play
-from settings import background, screen
-
-MainMenu()
--- flappy_ship/settings/screen.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk
-
-def size(w, h):
-    pass
-
-sw = Tk().winfo_screenwidth()
-sh = Tk().winfo_screenheight()
-framerate = 60
