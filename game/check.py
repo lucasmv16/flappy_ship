58395d5032d30053289514ccfda23b0222f173f7
Eduardo Silva <122567746+EduardoSilvaDiniz@users.noreply.github.com>
2023-07-03 17:04:40 -0300

    Update check.py

--- game/check.py
+++ game/check.py
@@ -4,9 +4,8 @@ import game
 from game.music import sound_effects, sound  # Sound effects and game music
 from menus.interface_ingame import GameoverMenu  # Game interfaces, such as the game over menu
 
-
+ ### fall death ###
 def fall():
-    ##fall death##
     if game.y_player >= game.max_y_player - 41 and game.gravity > 0:
         pygame.mixer.music.stop()
         sound_effects(sound["dead"])
